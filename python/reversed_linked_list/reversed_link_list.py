b25099f017665b51787d8480440ef2adba37f629
louis bemberg <louis.bemberg@gmail.com>
2021-09-09 20:03:16 -0700

    first linked list problem completed

--- python/reversed_linked_list/reversed_link_list.py
+++ python/reversed_linked_list/reversed_link_list.py
@@ -1,7 +1,25 @@
-# Given two lists [1,4,9,3,7] and [8,2,5,3,6] return 73941 + 63528 = 137469 => [9,6,4,7,3,1]
+# Given two LINKED LISTS [1,4,9,3,7] and [8,2,5,3,6] return 73941 + 63528 = 137469 => LINKED LIST [9,6,4,7,3,1]
+
 def add_two_numbers(l1, l2):
-    result = int(''.join(reversed([str(i) for i in l1]))) + int(''.join(reversed([str(i) for i in l2])))
-    return [int(x) for x in str(result)][::-1]
+    head = None
+    current = None
+    carry_over = 0
 
-
-print(add_two_numbers([1,4,9,3,7], [8,2,5,3,6]))
+    while l1 is not None or l2 is not None:
+        total_sum = carry_over
+        if l1 is not None:
+            total_sum += l1.val
+            l1 = l1.next
+        if l2 is not None:
+            total_sum += l2.val
+            l2 = l2.next
+        node = ListNode(total_sum % 10)
+        carry_over = total_sum // 10
+        if current is None:
+            current = head = node
+        else:
+            current.next = node
+            current = current.next
+    if carry_over > 0:
+        current.next = ListNode(carry_over)
+    return head
