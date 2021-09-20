abc3a7c58a9472f663803ab9874fa20b05ea5bdb
louis bemberg <louis.bemberg@gmail.com>
2021-09-19 23:08:40 -0700

    solved merging and medians, need to optimize by half

--- python/two_array_median/two_array_median.py
+++ python/two_array_median/two_array_median.py
@@ -3,39 +3,49 @@
 # Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
 
 # The overall run time complexity should be O(log (m+n)).
-import statistics
 
 def find_median(nums1, nums2):
+    import math
+
     size1 = len(nums1)
     size2 = len(nums2)
 
-    if size1 + size2 <= 4:
-        return statistics.median(nums1 + nums2)
-
-    nums3 = []
-    i, j = 0, 0
+    nums3 = [None] * (size1 + size2)
+    i, j, k = 0, 0, 0
     
+ 
     while i < size1 and j < size2:
         # merging operation
         if nums1[i] <= nums2[j]:
-            nums3.append(nums1[i])
+            nums3[k] = nums1[i]
             i += 1
         else:
-            nums3.append(nums2[j])
+            nums3[k] = nums2[j]
             j += 1
+        k += 1
 
-        # stop the merging as soon as we have the first half + 1 of the numbers
-        if len(nums3) > (size1 + size2)/2: break
+    # append any remaining leftovers
+    while i < size1:
+        nums3[k] = nums1[i]
+        i += 1
+        k += 1
+
+    while j < size2:
+        nums3[k] = nums2[j]
+        j += 1
+        k += 1
     
-    # implementation of median for odd/even numbers
-    return sum(nums3[-2:])/2 if (size1+size2 % 2 == 0) else nums3[-1]
+    if (size1 + size2) % 2 == 0:
+        return (nums3[int((size1 + size2) / 2)] + nums3[int((size1 + size2) / 2) - 1])/2
+    else:
+        return nums3[math.floor((size1 + size2) / 2)]
 
 
-print(find_median([1,3], [2]))
-print(find_median([1,2], [3,4]))
-print(find_median([0,0], [0,0]))
-print(find_median([], [1]))
-print(find_median([2], []))
-print(find_median([1,2,3,4], [1,2,2,3,4]))
-print(find_median([0,2,4,6], [1,3,5,7]))
-print(find_median([10], [5,6,7,8,11,12,13,14]))
+print(find_median([1,3], [2])) # should be 2
+print(find_median([1,2], [3,4])) # should be 2.5
+print(find_median([0,0], [0,0])) # should be 0
+print(find_median([], [1])) # should be 1
+print(find_median([2], [])) # should be 2
+print(find_median([1,2,3,4], [1,2,2,3,4])) # should be 2
+print(find_median([0,2,4,6], [1,3,5,7])) # should be 3.5
+print(find_median([10], [5,6,7,8,11,12,13,14])) # should be 
