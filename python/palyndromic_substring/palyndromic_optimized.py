30eafb2b637a8d832085233fc09b7e0d0c7e0255
louis bemberg <louis.bemberg@gmail.com>
2021-09-23 20:24:40 -0700

    solved using dynamic programming matrix, still too slow :/

--- python/palyndromic_substring/palyndromic_optimized.py
+++ python/palyndromic_substring/palyndromic_optimized.py
@@ -3,6 +3,8 @@
 # Dynamic programming approach
 def longestPalindrome(s):
     n = len(s)
+    max_pal_length = 1
+    max_start = 0
 
     # create a n x n dynamic programming table (or matrix):
     table = [[0 for x in range(n)] for y in range(n)]
@@ -15,6 +17,8 @@ def longestPalindrome(s):
     for i in range(0, n  - 1):
         if s[i] == s[i + 1]: # palindrome of size 2 check
             table[i][i+1] = 1
+            max_pal_length = 2
+            max_start = i
 
     # check for any bigger palindrome:
     size = 3
@@ -23,27 +27,23 @@ def longestPalindrome(s):
         while i < (n - size + 1):
             j = i + size - 1
 
-            if table[i+1][j-1] and s[i] == s[j]:
-                table[i][j] == 1
+            if table[i+1][j-1] == 1 and s[i] == s[j]:
+                table[i][j] = 1
+                if size > max_pal_length:
+                    max_start = i
+                    max_pal_length = size
             i += 1
         size += 1
+    # print(table)
+    print('your palindrome starts with', s[max_start], 'at index', max_start, 'and is', max_pal_length, 'long')
+    return s[max_start:max_start + max_pal_length]
 
-
-    print(table)
-
-def palindrome(string):
-    return string.lower() == string[::-1].lower()
-
-
-
-palindrome('hello') # False
-palindrome('hahah') # True
-
-# longestPalindrome('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-longestPalindrome("ABBA") # ABBA
-# longestPalindrome("cbbd") # bb
-# longestPalindrome("bb") # bb
-# longestPalindrome("OcaracA") # carac
-# longestPalindrome("a") # a
-# longestPalindrome("babad") # both  bab and aba are valid
-# longestPalindrome("ycaracaracx") # caracarac
+print(longestPalindrome('ABCDEFGHIJKLMNOPQRSTUVWXYZ')) # A or Z
+print(longestPalindrome("ABBA")) # ABBA
+print(longestPalindrome("cbbd")) # bb
+print(longestPalindrome("bb")) # bb
+print(longestPalindrome("OcaracA")) # carac
+print(longestPalindrome("a")) # a
+print(longestPalindrome("babad")) # both  bab and aba are valid
+print(longestPalindrome("ycaracaracx")) # caracarac
+print(longestPalindrome("babad")) # bab or aba
